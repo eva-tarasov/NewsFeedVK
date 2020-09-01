ada13860c722cb39bd68e49f98a14a244ddd5974
Egeniy Tarasov <tarasov@ucontent.ru>
2020-09-01 14:26:30 +0300

    Initial Commit

---
+++ NewsFeedVK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFeedVK
+//
+//  Created by Евгений Тарасов on 01.09.2020.
+//  Copyright © 2020 Евгений Тарасов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
